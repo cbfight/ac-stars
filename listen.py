0dd9bce47f36fe657daa5c5c25e6c18f77f6d015
wesley <wesleyxwong@gmail.com>
2020-06-04 15:06:34 -0400

    graphing

--- listen.py
+++ listen.py
@@ -2,6 +2,8 @@ import sounddevice
 from matplotlib import pyplot as plt
 import numpy as np
 import time
+import queue
+from matplotlib.animation import FuncAnimation
 
 class listener():
     def __init__(self):
@@ -9,13 +11,34 @@ class listener():
         self.sr = 44100 #sample rate in hertz
         self.bs = 512 #chunk size
         self.stop_time = time.time()+5
-        self.stream = sounddevice.Stream(samplerate=self.sr, blocksize= self.bs)
-
+        self.stream = sounddevice.InputStream(samplerate=self.sr, blocksize=self.bs, callback=self.callback)
+        self.q = queue.Queue()
+        self.init_data = np.zeros((self.bs,1))
+        self.fig, self.ax = plt.subplots()
+        self.trace = self.ax.plot(self.init_data)
+        self.animation = FuncAnimation(self.fig, self.update_plot, blit=True)
+    def start_listening(self):
         with self.stream:
+            plt.show()
             while time.time() < self.stop_time:
                 time.sleep(1)
+    def callback(self, indata, frames, time, status):
+        'recieves chunks from stream, puts in queue'
+        self.q.put(indata.copy())
+    def update_plot(self,frame):
+        while True:
+            try:
+                data = self.q.get_nowait()
+            except queue.Empty:
+                break
+            for column, line in enumerate(self.trace):
+                line.set_ydata(data[:,0])
+        return self.trace
+
+        
 
 
 foo = listener()
-foo
-print("done!")
+print('listening!')
+foo.start_listening()
+print("done listening!")
