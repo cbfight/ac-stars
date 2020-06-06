dbe3cfe8bea05fd6394b0244adbacf7b25c3cd0d
wesley <wesleyxwong@gmail.com>
2020-06-06 19:34:04 -0400

    made it use multiprocessing

--- listen.py
+++ listen.py
@@ -1,44 +1,70 @@
-import sounddevice
+import sounddevice as sd
 from matplotlib import pyplot as plt
 import numpy as np
 import time
-import queue
 from matplotlib.animation import FuncAnimation
+from multiprocessing import Process, Queue
+import queue as not_mp_q
 
-class listener():
-    def __init__(self):
-        self.duration = 5 #seconds
-        self.sr = 44100 #sample rate in hertz
-        self.bs = 512 #chunk size
-        self.stop_time = time.time()+5
-        self.stream = sounddevice.InputStream(samplerate=self.sr, blocksize=self.bs, callback=self.callback)
-        self.q = queue.Queue()
-        self.init_data = np.zeros((self.bs,1))
-        self.fig, self.ax = plt.subplots()
-        self.trace = self.ax.plot(self.init_data)
-        self.animation = FuncAnimation(self.fig, self.update_plot, blit=True)
-    def start_listening(self):
-        with self.stream:
-            plt.show()
-            while time.time() < self.stop_time:
-                time.sleep(1)
-    def callback(self, indata, frames, time, status):
-        'recieves chunks from stream, puts in queue'
-        self.q.put(indata.copy())
-    def update_plot(self,frame):
-        while True:
-            try:
-                data = self.q.get_nowait()
-            except queue.Empty:
-                break
-            for column, line in enumerate(self.trace):
-                line.set_ydata(data[:,0])
-        return self.trace
+duration = 5000 #milliseconds
+fig, ax, trace = 0,0,0 #initializing?
 
-        
+def main():
+    print('main method')
+    stack = Queue()
+    q = Process(target = listen_handler, args=(stack,))
+    z = Process(target = plot_handler, args=(stack,))
+    z.start()
+    q.start()
+    q.join()
+    z.join()
 
+def plot_handler(q):
+    global stack
+    stack = q
+    plotter()
 
-foo = listener()
-print('listening!')
-foo.start_listening()
-print("done listening!")
+def listen_handler(q):
+    global stack
+    stack = q
+    streaming_input()
+
+def plotter():
+    global fig, ax, trace
+    print('plotter method')
+    fig, ax = plt.subplots()
+    trace = ax.plot(np.zeros((512,1)))
+    animated = FuncAnimation(fig, animate_plot,blit=True)
+    plt.show()
+
+def animate_plot(frame):
+    global stack
+    while True:
+        try:
+            data = stack.get_nowait()
+        except not_mp_q.Empty:
+            print('queue is empty!')
+            break
+        for column,line in enumerate(trace):
+            line.set_ydata(data[:,0])
+            print('shoulda updated plot now')
+    return trace
+
+def stream_callback(inframe, framecount, time, status):
+    global stack
+    stack.put(inframe)
+    #print(stack.qsize())
+    
+def streaming_input():
+    print('streaming input method')
+    sound_in = sd.InputStream(samplerate=44100,
+                              blocksize=512,
+                              device=0,
+                              channels=1,
+                              dtype=np.float32,
+                              callback=stream_callback)
+    with sound_in:
+       sd.sleep(duration) #5 seconds duration
+
+if __name__ == '__main__':
+    main()
